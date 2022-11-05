bec0c9f465416086a47e3764d7856a3dd251554e
malkiAbdoo <abdo.malkiep@gmail.com>
2022-11-05 15:34:04 +0100

    Removing the reset function.

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,40 @@
-import html2canvas from "html2canvas";
+import html2canvas from 'html2canvas';
 
 const body = document.body;
-
-// reset the cursor
-export const reset = (tool: HTMLElement) => {
-  tool.remove();
-  if (tool.tagName.toLowerCase() === 'canvas') {
-    const canvas = tool as HTMLCanvasElement;
-    const ctx = canvas.getContext('2d');
-    if (ctx) ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
-}
 
 // create a trailer
 function createTrailer(element = body) {
   const trailer = document.createElement('div');
-  trailer.setAttribute('style', 
-    "display: none;position: fixed;translate: -50% -50%;pointer-events: none !important;z-index: 1999;"
+  trailer.setAttribute(
+    'style',
+    'display: none;position: fixed;translate: -50% -50%;pointer-events: none !important;z-index: 1999;'
   );
-  element.addEventListener('mouseleave', () => trailer.style.display = "none");
+  element.addEventListener(
+    'mouseleave',
+    () => (trailer.style.display = 'none')
+  );
   element.addEventListener('mousemove', event => {
     trailer.style.top = event.pageY - window.scrollY + 'px';
     trailer.style.left = event.pageX - window.scrollX + 'px';
-    trailer.style.display = "block";
+    trailer.style.display = 'block';
   });
   return trailer;
 }
 
-
-export function focus (element: HTMLElement = body): HTMLElement {
+export function focus(element: HTMLElement = body) {
   const focusBox = document.createElement('div');
   const focusCircle = createTrailer(element);
-  focusBox.classList.add("cursor-focus-box");
+  focusBox.classList.add('cursor-focus-box');
   focusBox.appendChild(focusCircle);
   body.appendChild(focusBox);
   const containerRect = element.getBoundingClientRect();
   const boxPosition = {
     x: containerRect.left + window.scrollX,
     y: containerRect.top + window.scrollY
-  }
-  focusBox.setAttribute('style',
-    "position: absolute;z-index: 1999;mix-blend-mode: multiply;pointer-events: none !important;"
+  };
+  focusBox.setAttribute(
+    'style',
+    'position: absolute;z-index: 1999;mix-blend-mode: multiply;pointer-events: none !important;'
   );
   focusBox.style.width = containerRect.width + 'px';
   focusBox.style.height = containerRect.height + 'px';
@@ -48,14 +42,17 @@ export function focus (element: HTMLElement = body): HTMLElement {
   focusBox.style.top = boxPosition.y + 'px';
   focusCircle.style.background = '#fff';
 
-  window.addEventListener("resize", () => {
+  window.addEventListener('resize', () => {
     const newRect = element.getBoundingClientRect();
     focusBox.style.width = newRect.width + 'px';
     focusBox.style.height = newRect.height + 'px';
   });
 
   let style = window.getComputedStyle(focusBox);
-  if (style['opacity'] === '1' && style['backgroundColor'] === 'rgba(0, 0, 0, 0)') {
+  if (
+    style['opacity'] === '1' &&
+    style['backgroundColor'] === 'rgba(0, 0, 0, 0)'
+  ) {
     focusBox.style.opacity = '0.5';
     focusBox.style.backgroundColor = '#000';
   }
@@ -69,14 +66,21 @@ export function focus (element: HTMLElement = body): HTMLElement {
 }
 focus(document.querySelector('.s2') as HTMLElement);
 
-export function addTrailer (duration = 200, element: HTMLElement = body): HTMLElement {
+export function addTrailer(
+  duration = 200,
+  element: HTMLElement = body
+): HTMLElement {
   const trailer = createTrailer(element);
-  trailer.classList.add("cursor-trailer");
+  trailer.classList.add('cursor-trailer');
   body.appendChild(trailer);
   const style = window.getComputedStyle(trailer);
   trailer.style.transitionDuration = duration + 'ms';
   trailer.style.transitionTimingFunction = 'ease-out';
-  if (style['width'] == 'auto' && style['height'] == 'auto' && style['borderWidth'] == '0px') {
+  if (
+    style['width'] == 'auto' &&
+    style['height'] == 'auto' &&
+    style['borderWidth'] == '0px'
+  ) {
     trailer.style.width = '30px';
     trailer.style.height = '30px';
     trailer.style.borderRadius = '50%';
@@ -86,9 +90,9 @@ export function addTrailer (duration = 200, element: HTMLElement = body): HTMLEl
 }
 addTrailer(200, document.querySelector('.s1') as HTMLElement);
 
-export function zoomLens (zoomRatio = 1.5, element: HTMLElement = body): HTMLElement {
-  const lens = document.createElement("div");
-  lens.classList.add("cursor-lens");
+export function zoomLens(zoomRatio = 1.5, element: HTMLElement = body) {
+  const lens = document.createElement('div');
+  lens.classList.add('cursor-lens');
   lens.style.position = 'fixed';
   lens.style.pointerEvents = 'none !important';
   body.appendChild(lens);
@@ -108,28 +112,32 @@ export function zoomLens (zoomRatio = 1.5, element: HTMLElement = body): HTMLEle
     if (element.tagName.toLowerCase() === 'img')
       lens.style.backgroundImage = `url(${(element as HTMLImageElement).src})`;
     // take a screenshot
-    else html2canvas(element).then(canvas => {
-      lens.style.backgroundImage = `url(${canvas.toDataURL()})`;
-    });
+    else
+      html2canvas(element).then(canvas => {
+        lens.style.backgroundImage = `url(${canvas.toDataURL()})`;
+      });
   }
   setBackgroundImage();
 
-  window.addEventListener("resize", setBackgroundImage);
-  element.addEventListener("mouseleave", () => lens.style.display = "none");
-  element.addEventListener("mousemove", (event) => {
+  window.addEventListener('resize', setBackgroundImage);
+  element.addEventListener('mouseleave', () => (lens.style.display = 'none'));
+  element.addEventListener('mousemove', event => {
     const containerRect = element.getBoundingClientRect();
     const { x, y } = mousePosition(event, containerRect);
     const cx = lensRect.width / zoomRect.width;
     const cy = lensRect.height / zoomRect.height;
-  
-    lens.style.backgroundSize = containerRect.width * cx + 'px ' + containerRect.height * cy + 'px';
+
+    lens.style.backgroundSize =
+      containerRect.width * cx + 'px ' + containerRect.height * cy + 'px';
     lens.style.backgroundPosition = '-' + x * cx + 'px -' + y * cy + 'px';
   });
 
   function mousePosition(event: MouseEvent, containerRect: DOMRect) {
-    let x = event.pageX - window.scrollX - containerRect.left - zoomRect.width / 2;
-    let y = event.pageY - window.scrollY - containerRect.top - zoomRect.height / 2;
-    
+    let x =
+      event.pageX - window.scrollX - containerRect.left - zoomRect.width / 2;
+    let y =
+      event.pageY - window.scrollY - containerRect.top - zoomRect.height / 2;
+
     let maxX = containerRect.width - zoomRect.width;
     let maxY = containerRect.height - zoomRect.height;
     if (x <= 0) x = 0;
@@ -139,8 +147,8 @@ export function zoomLens (zoomRatio = 1.5, element: HTMLElement = body): HTMLEle
 
     maxX = containerRect.width - lensRect.width;
     maxY = containerRect.height - lensRect.height;
-    let lensX = x + zoomRect.width/2 - lensRect.width/2;
-    let lensY = y + zoomRect.height/2 - lensRect.height/2;
+    let lensX = x + zoomRect.width / 2 - lensRect.width / 2;
+    let lensY = y + zoomRect.height / 2 - lensRect.height / 2;
     if (lensX <= 0) lensX = 0;
     else if (lensX >= maxX) lensX = maxX;
     if (lensY <= 0) lensY = 0;
@@ -148,7 +156,7 @@ export function zoomLens (zoomRatio = 1.5, element: HTMLElement = body): HTMLEle
 
     lens.style.left = lensX + containerRect.left + 'px';
     lens.style.top = lensY + containerRect.top + 'px';
-    lens.style.display = "block";
+    lens.style.display = 'block';
 
     return { x, y };
   }
@@ -160,15 +168,18 @@ export interface context {
   lineWidth?: number;
   color?: string;
 }
-export function drawingMode (element: HTMLElement = body, options: context = {lineWidth: 5, color: '#000'}): HTMLElement {
-  const canvas = document.createElement("canvas");
+export function drawingMode(
+  element: HTMLElement = body,
+  options: context = { lineWidth: 5, color: '#000' }
+) {
+  const canvas = document.createElement('canvas');
   const containerRect = element.getBoundingClientRect();
   const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-  const elementPosition = { 
+  const elementPosition = {
     x: containerRect.left + window.scrollX,
     y: containerRect.top + window.scrollY
-  }
-  
+  };
+
   // styling
   canvas.style.position = 'absolute';
   canvas.style.zIndex = '1999';
@@ -178,17 +189,16 @@ export function drawingMode (element: HTMLElement = body, options: context = {li
   canvas.height = containerRect.height;
   body.appendChild(canvas);
 
-  
   let isDrawing = false;
-  function start (e: MouseEvent) {
+  function start(e: MouseEvent) {
     isDrawing = true;
     draw(e);
   }
-  function finish () {
+  function finish() {
     isDrawing = false;
     ctx.beginPath();
   }
-  function draw (event: MouseEvent) {
+  function draw(event: MouseEvent) {
     if (!isDrawing) return;
 
     const x = event.pageX - elementPosition.x;
@@ -196,34 +206,25 @@ export function drawingMode (element: HTMLElement = body, options: context = {li
 
     ctx.lineWidth = options.lineWidth || 5;
     ctx.strokeStyle = options.color || '#000';
-    ctx.lineCap = "round";
+    ctx.lineCap = 'round';
 
     ctx.lineTo(x, y);
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(x, y);
   }
-  function resize () {
+  function resize() {
     const newRect = element.getBoundingClientRect();
     canvas.width = newRect.width;
     canvas.height = newRect.height;
   }
 
-  canvas.addEventListener("mousedown", start);
-  canvas.addEventListener("mousemove", draw);
-  canvas.addEventListener("mouseup", finish);
-  canvas.addEventListener("mouseleave", finish);
-  window.addEventListener("resize", resize);
+  canvas.addEventListener('mousedown', start);
+  canvas.addEventListener('mousemove', draw);
+  canvas.addEventListener('mouseup', finish);
+  canvas.addEventListener('mouseleave', finish);
+  window.addEventListener('resize', resize);
 
   return canvas;
 }
-drawingMode(document.querySelector(".s4") as HTMLElement, {color: '#ffff00'});
-
-// Scroll cursor
-
-
-
-
-
-
-
+drawingMode(document.querySelector('.s4') as HTMLElement, { color: '#ffff00' });
