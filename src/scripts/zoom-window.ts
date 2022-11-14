3fa24939799d1669456a94740eff867d5b75f086
malkiAbdoo <abdo.malkiep@gmail.com>
2022-11-14 21:51:18 +0100

    Fix the zoom window.

--- src/scripts/zoom-window.ts
+++ src/scripts/zoom-window.ts
@@ -4,18 +4,16 @@ export function createZoomWindow(lens: HTMLDivElement, image: Img) {
   const zoomWindow = document.createElement('div');
   const lensRect = lens.getBoundingClientRect();
   zoomWindow.classList.add('zoom-window');
+  document.body.appendChild(zoomWindow);
   zoomWindow.style.position = 'absolute';
   zoomWindow.style.pointerEvents = 'none';
-  document.body.appendChild(zoomWindow);
+  zoomWindow.style.aspectRatio = lensRect.width + '/' + lensRect.height;
 
   const rect = zoomWindow.getBoundingClientRect();
   if (rect.width == 0) {
-    zoomWindow.style.width = image.width + 'px';
-    zoomWindow.style.aspectRatio = lensRect.width + '/' + lensRect.height;
-  }
-  if (rect.left == 0) {
     zoomWindow.style.top = image.offsetTop + 'px';
     zoomWindow.style.left = image.offsetLeft + image.width + 10 + 'px';
+    zoomWindow.style.width = image.width / 2 + 'px';
   }
   //   zoomWindow.style.display = 'none';
 
